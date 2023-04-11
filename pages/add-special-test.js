1fb7eebdb1c9b3e7e69ff5f76f83776e0ecc6971
IbrahimG88 <goharyibrahim@gmail.com>
2023-04-11 16:20:01 +0200

    first commit

---
+++ pages/add-special-test.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import Select from "react-select";
+import { fetcher } from "../lib/fetcher";
+
+export default function AddSpecialTest() {
+  const [specialTestName, setSpecialTestName] = useState("");
+  const [connectedToTestOnLIS, setConnectedToTestOnLIS] = useState("");
+  const [testsList, setTestsList] = useState([]);
+
+  useEffect(() => {
+    const fetchTestsList = async () => {
+      const data = await fetcher("/api/get-testslist");
+      if (data) {
+        setTestsList(data);
+        console.log("testsList", data);
+      } else {
+        console.error("Error fetching tests list from database");
+      }
+    };
+
+    if (testsList.length === 0) {
+      fetchTestsList();
+    }
+  }, [testsList]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(specialTestName, connectedToTestOnLIS);
+  };
+
+  return (
+    <form className="flex flex-col space-y-4 p-4" onSubmit={handleSubmit}>
+      <label htmlFor="special-test-name" className="text-lg font-medium">
+        Special Test Name
+      </label>
+      <input
+        type="text"
+        id="special-test-name"
+        value={specialTestName}
+        onChange={(e) => setSpecialTestName(e.target.value)}
+        className="border border-gray-300 rounded-md shadow-sm p-2"
+      />
+
+      <label htmlFor="connected-to-test-on-lis" className="text-lg font-medium">
+        Connected to test on LIS
+      </label>
+      <Select
+        options={testsList.map((test) => ({
+          id: test.id,
+          value: test.testName,
+          label: test.testName,
+        }))}
+        value={{ value: connectedToTestOnLIS, label: connectedToTestOnLIS }}
+        onChange={(option) => setConnectedToTestOnLIS(option.value)}
+        isSearchable
+        className="border border-gray-300 rounded-md shadow-sm p-2"
+      />
+
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mx-auto focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
---
+++ pages/api/get-testslist.js
@@ -0,0 +1,25 @@
+import { connectToDatabase } from "../../lib/db";
+
+export default async function handler(req, res) {
+  try {
+    if (req.method !== "GET") {
+      throw { status: 400, message: "Invalid request method" };
+    }
+
+    const client = await connectToDatabase();
+    const db = client.db("myFirstDatabase");
+    const collection = db.collection("inventory2");
+
+    const existingTestsList = await collection.findOne({});
+    if (!existingTestsList) {
+      return res.status(404).json({ message: "Tests list not found" });
+    }
+
+    client.close();
+    return res.status(200).json(existingTestsList.testsList);
+  } catch (error) {
+    console.error(error);
+    const { status = 500, message = "Something went wrong" } = error;
+    return res.status(status).json({ message });
+  }
+}
